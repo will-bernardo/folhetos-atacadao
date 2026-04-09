8da88e2c9140a547b8f12ea27a4123e22c384535
William Bernardo <bernardowill175@gmail.com>
2026-04-08 22:59:33 -0300

    remove mock de panfleto

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -13,13 +13,13 @@ export interface Config {
 }
 
 export const folhetos: Folheto[] = [
-  {
-    id: '1',
-    titulo: 'Ofertas',
-    url: 'https://pub-218438f0586c4a8a9d966a4295cde53e.r2.dev/07-04-2026.pdf',
-    thumbnail: 'https://pub-218438f0586c4a8a9d966a4295cde53e.r2.dev/miniatura-pdf.webp',
-    data: '2026-04-08'
-  }
+  // {
+  //   id: '1',
+  //   titulo: 'Ofertas',
+  //   url: 'https://pub-218438f0586c4a8a9d966a4295cde53e.r2.dev/07-04-2026.pdf',
+  //   thumbnail: 'https://pub-218438f0586c4a8a9d966a4295cde53e.r2.dev/miniatura-pdf.webp',
+  //   data: '2026-04-08'
+  // }
 ];
 
 export const config: Config = {
