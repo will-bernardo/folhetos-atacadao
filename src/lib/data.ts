eee700c6466d7675bdea5ee8abacd44531171a7f
William Bernardo <bernardowill175@gmail.com>
2026-04-09 10:09:00 -0300

    add offer

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -19,6 +19,20 @@ export const folhetos: Folheto[] = [
     url: 'https://pub-218438f0586c4a8a9d966a4295cde53e.r2.dev/dia-A.pdf',
     thumbnail: 'https://pub-218438f0586c4a8a9d966a4295cde53e.r2.dev/thumb-dia-a.avif',
     data: '2026-04-09'
+  },
+  {
+    id: '2',
+    titulo: 'HORTIFRUTI',
+    url: 'https://pub-218438f0586c4a8a9d966a4295cde53e.r2.dev/hortifruti.pdf',
+    thumbnail: 'https://pub-218438f0586c4a8a9d966a4295cde53e.r2.dev/thumb-hortifruti.avif',
+    data: '2026-04-09'
+  },
+  {
+    id: '3',
+    titulo: 'AÇOUGUE, PADARIA E FATIADOS',
+    url: 'https://pub-218438f0586c4a8a9d966a4295cde53e.r2.dev/paf.pdf',
+    thumbnail: 'https://pub-218438f0586c4a8a9d966a4295cde53e.r2.dev/thumb-paf.avif',
+    data: '2026-04-09'
   }
 ];
 
